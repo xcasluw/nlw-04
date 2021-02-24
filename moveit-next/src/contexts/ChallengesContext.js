3d322e5570646a769126cecebcf70925c5f03077
xcasl <oligena@hotmail.com>
2021-02-24 10:32:43 -0300

    Day three - Working with Context API

---
+++ moveit-next/src/contexts/ChallengesContext.js
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react";
+import challenges from "../../challenges.json";
+
+export const ChallengesContext = createContext({});
+
+export function ChallengesProvider({ children }) {
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
+  function levelUp() {
+    setLevel(level + 1);
+  }
+
+  function startNewChallenge() {
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
+  }
+
+  function resetChallenge() {
+    setActiveChallenge(null);
+  }
+
+  return (
+    <ChallengesContext.Provider value={{ level, currentExperience, challengesCompleted, levelUp, startNewChallenge, activeChallenge, resetChallenge, experienceToNextLevel }}>
+      {children}
+    </ChallengesContext.Provider>
+  )
+}
